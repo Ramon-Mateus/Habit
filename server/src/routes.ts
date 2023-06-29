a3bfa00f1c29ad685ba871ba353ed58b593aa6b6
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2023-06-29 13:42:04 -0300

    rota de detalhe do dia adicionada

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -29,5 +29,47 @@ export async function appRoutes(app: FastifyInstance) {
             }
         }
     })
-    })  
+    })
+
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query)
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day')
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date, // data menor ou igual da data especificada
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay,  
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate(),
+            },
+            include: {
+                dayHabits: true
+            }
+        })
+        
+        const coompletedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+
+        return {
+            possibleHabits,
+            coompletedHabits
+        }
+    })
 }
