059f66b5b17094c5d9d477d6eaabdc308526f307
Ramon de Oliveira Araújo Mateus <60753390+Ramon-Mateus@users.noreply.github.com>
2023-07-15 13:53:05 -0300

    marcar ou desmarcar hábito como concluído

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -72,4 +72,56 @@ export async function appRoutes(app: FastifyInstance) {
             coompletedHabits
         }
     })
+
+    app.patch('/habits/:id/toggle', async (request) => {
+
+        const toggleHabitParams = z.object({
+            id: z.string().uuid(),
+        })
+
+        const { id } = toggleHabitParams.parse(request.params)
+
+        const today = dayjs().startOf('day').toDate()
+
+        let day = await prisma.day.findUnique({
+            where: {
+                date: today,
+            }
+        })
+
+        // vai criar o hábito, caso ele não exista, pois day não vai ser criado se não tiver nenhum hábito completando naquele dia
+        if (!day) {
+            day = await prisma.day.create({
+                data: {
+                    date: today,
+                }
+            })
+        }
+
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id: id
+                }
+            }
+        })
+
+        if (dayHabit) {
+            // remover a marcação de completo
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id,
+                }
+            })
+        } else {
+            // completar o hábito
+            await prisma.dayHabit.create({
+                data: {
+                    day_id: day.id,
+                    habit_id: id,
+                }
+            })
+        }
+    })
 }
